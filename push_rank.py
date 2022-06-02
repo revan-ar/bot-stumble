70be2870dad88cdeb93419f6572b7668305d1fa9
Revan AR <minicode.idn@gmail.com>
2022-06-03 00:53:19 +0700

    update fitur

--- push_rank.py
+++ push_rank.py
@@ -3,6 +3,11 @@ import requests
 import os
 import time, json, datetime
 
+print('-- Tipe Ronde --')
+print('1. Ronde 3 (Tereliminasi)')
+print('2. Juara 1 (Mahkota)')
+ronde = input('Pilih ronde: ')
+time.sleep(1)
 total_thread = os.cpu_count()
 time.sleep(1)
 loop = 1
@@ -13,7 +18,11 @@ def push_rank():
 	}
 	while loop == 1:
 		jam = datetime.datetime.now()
-		req = requests.get('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/2', headers=headers)
+		if ronde == '1':
+			juara = 2
+		else:
+			juara = 3
+		req = requests.get('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/{}'.format(juara), headers=headers)
 		response = json.loads(req.text)
 		print('[{}-{}-{} {}:{}:{}] SUCCESS |TROPI: {} |MAHKOTA: {}'.format(jam.year, jam.month, jam.day, jam.hour, jam.minute, jam.second, response['User']['SkillRating'], response['User']['Crowns']))
 		time.sleep(0.05)
